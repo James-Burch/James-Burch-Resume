8057dd289c68fcc367ee0780b5ff150963260b66
James-Burch <161542248+James-Burch@users.noreply.github.com>
2025-03-05 01:27:23 +0000

    Install and uninstall tailwind css

--- James-Burch-Resume/src/components/Navbar.jsx
+++ James-Burch-Resume/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+        <nav className="flex justify-between items-center p-4 bg-gray-900 text-white">
+          <h1 className="text-xl font-bold">My Portfolio</h1>
+          <ul className="flex space-x-4">
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      );
+};
 
-export default Navbar
+export default Navbar;
